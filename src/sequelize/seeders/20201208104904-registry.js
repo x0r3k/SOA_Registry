08de9c73401a2c2d1cda774ea69c6058e31354ee
x0r3k <kir221dol@gmail.com>
2020-12-12 16:50:16 +0200

    add script for dockerized services

--- src/sequelize/seeders/20201208104904-registry.js
+++ src/sequelize/seeders/20201208104904-registry.js
@@ -1,46 +1,88 @@
 'use strict';
 
+const dockerData = [
+  {
+    id: 1,
+    code: "auth",
+    name: "Auth user service",
+    description: '',
+    url: 'http://localhost:8082',
+    port: '8082',
+    status: 'on',
+  },
+  {
+    id: 2,
+    code: "product",
+    name: "Product service",
+    description: '',
+    url: 'http://localhost:8072',
+    port: '8072',
+    status: 'on'
+  },
+  {
+    id: 3,
+    code: "car",
+    name: "Car service",
+    description: '',
+    url: 'http://localhost:8062',
+    port: '8062',
+    status: 'on'
+  },
+  {
+    id: 4,
+    code: "order",
+    name: "Order service",
+    description: '',
+    url: 'http://localhost:8052',
+    port: '8052',
+    status: 'on'
+  },
+];
+
+const devData = [
+  {
+    id: 1,
+    code: "auth",
+    name: "Auth user service",
+    description: '',
+    url: 'http://localhost:8081',
+    port: '8081',
+    status: 'on',
+  },
+  {
+    id: 2,
+    code: "product",
+    name: "Product service",
+    description: '',
+    url: 'http://localhost:8071',
+    port: '8071',
+    status: 'on'
+  },
+  {
+    id: 3,
+    code: "car",
+    name: "Car service",
+    description: '',
+    url: 'http://localhost:8061',
+    port: '8061',
+    status: 'on'
+  },
+  {
+    id: 4,
+    code: "order",
+    name: "Order service",
+    description: '',
+    url: 'http://localhost:8051',
+    port: '8051',
+    status: 'on'
+  },
+];
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
 
-    await queryInterface.bulkInsert('registry', [
-      {
-        id: 1,
-        code: "auth",
-        name: "Auth user service",
-        description: '',
-        url: 'http://localhost:8081',
-        port: '8081',
-        status: 'on',
-      },
-      {
-        id: 2,
-        code: "product",
-        name: "Product service",
-        description: '',
-        url: 'http://localhost:8071',
-        port: '8071',
-        status: 'on'
-      },
-      {
-        id: 3,
-        code: "car",
-        name: "Car service",
-        description: '',
-        url: 'http://localhost:8061',
-        port: '8061',
-        status: 'on'
-      },
-      {
-        id: 4,
-        code: "order",
-        name: "Order service",
-        description: '',
-        url: 'http://localhost:8051',
-        port: '8051',
-        status: 'on'
-      },
-  ], {});
+    await queryInterface.bulkInsert('registry', process.env.NODE_ENV === 'docker' ? dockerData : devData
+    , {});
 
   },
 
